b1df293285be65118cdf485f4d3a20125c264ffb
pikaachu09 <72344695+pikaachu09@users.noreply.github.com>
2020-10-04 18:46:15 +0530

    Create Words.c

---
+++ Words.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+      char s[150];
+      int i, word=1;
+
+      printf("Enter a string: ");
+      scanf("%[^\n]",s);
+
+      for(i = 0; s[i] != '\0'; ++i)
+      {
+          if(s[i]==' '&& s[i+1]!=' ') word++;
+      }
+
+      if(s[i-1]==' ')word--;
+      if(s[0]==' ')word--;
+      if(s[0]=='\0')word--;
+
+      printf("Word: %d",word);
+      return 0;
+}
